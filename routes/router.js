2069e2648ae29818431a7163931a3054cb6714cd
Nacho Prado <fonsvitae.spain@gmail.com>
2023-07-13 14:10:17 +0200

    routes and login

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,16 @@
 import {Router} from "express";
+// import lista_router from "../routes/lista_router.js";
+import login_router from "../routes/login_router.js";
+// import showroom_router from "../routes/showroom_router.js";
+// import registro_router from "../routes/registro_router.js";
 
 const router = Router();
 
-router.get  ("/" , (req, res) => {
-    console.log (req);
+router.use("/", login_router);
+// //router.use("/cancion",cancionRouter);
+// router.use("/",usuarioRouter);
 
-    res.send("Hello World");
-});
+// router.use("/api/cancion",jwtMiddleware,cancionApiRouter);
+
 
 export default router;
