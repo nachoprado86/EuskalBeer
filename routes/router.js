58702a67fa8bd6fa34d60484cc117206421b958e
Nacho Prado <fonsvitae.spain@gmail.com>
2023-07-13 16:21:42 +0200

    creadas y funcionando todas las rutas

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,15 @@
 import {Router} from "express";
-// import lista_router from "../routes/lista_router.js";
+import lista_router from "../routes/lista_router.js";
 import login_router from "../routes/login_router.js";
-// import showroom_router from "../routes/showroom_router.js";
-// import registro_router from "../routes/registro_router.js";
+import showroom_router from "../routes/showroom_router.js";
+import registro_router from "../routes/registro_router.js";
 
 const router = Router();
 
 router.use("/", login_router);
-// //router.use("/cancion",cancionRouter);
-// router.use("/",usuarioRouter);
-
-// router.use("/api/cancion",jwtMiddleware,cancionApiRouter);
+router.use("/register", registro_router);
+router.use("/showroom", showroom_router);
+router.use("/list", lista_router);
 
 
 export default router;
