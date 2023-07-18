74909a4cb6189b3ef5de1f8a3b1e5ce6d5ed7076
Ruben Moreno <134232975+rubenmmoreno@users.noreply.github.com>
2023-07-19 00:28:24 +0200

    LISTA intento fallido

--- controllers/cerveza/lista_controller.js
+++ controllers/cerveza/lista_controller.js
@@ -22,4 +22,38 @@ async function borrarFavorito (req, res) {
     res.redirect("/showroom");
 }
 
-export {renderLista, crearFavorito, borrarFavorito,};
+/* async function getAll(req, res) {
+    try {
+      // Obtener los datos de las cervezas
+      let cervezasData = await cerveza_controller.getAll();
+  
+      // Crear un arreglo para almacenar los datos de las cervezas
+      let cervezas = [];
+  
+      // Recorrer los datos de las cervezas y extraer las propiedades deseadas
+      cervezasData.forEach(cerveza => {
+        cervezas.push({
+          id_cerveza: cerveza.dataValues.id_cerveza,
+          nombre_cerveza: cerveza.dataValues.nombre_cerveza,
+          clase: cerveza.dataValues.clase,
+          marca: cerveza.dataValues.marca,
+          origen: cerveza.dataValues.origen,
+          graduacion: cerveza.dataValues.graduacion
+        });
+      });
+  
+      // Obtener los datos de la sesión
+      const usuario = req.session.usuario;
+  
+      // Renderizar la vista Pug y pasar los datos
+      res.render("lista_view", { cervezas: cervezas, usuario: usuario });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error en el servidor");
+    }
+  } */
+  
+  
+
+
+export {renderLista, crearFavorito, borrarFavorito};
